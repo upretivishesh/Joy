fae8aee9a04062b7fcd138cbaa66606a9d06cd81
Vishesh Upreti <108606099+upretivishesh@users.noreply.github.com>
2026-04-08 19:41:45 +0530

    Create email_utils.py

---
+++ email_utils.py
@@ -0,0 +1,18 @@
+def generate_email(name):
+    return f"""
+Hi {name},
+
+Thank you for your interest.
+
+Please share the following details:
+
+1. Current CTC
+2. Expected CTC
+3. Current Location
+4. Notice Period
+5. Total Experience
+6. Reason for Change
+
+Regards,  
+Seven Hiring
+"""
